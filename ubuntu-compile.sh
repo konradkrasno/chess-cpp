ceb4ba9f5f724a0fdbd2af20bbab894a1c545777
konradkrasno <konradkrasno@gmail.com>
2020-05-27 14:28:17 +0200

    compile file added

---
+++ ubuntu-compile.sh
@@ -0,0 +1,2 @@
+g++ chess.cpp subsidiary.cpp BoardAttributes.cpp UpdateAttributes.cpp ChessMan.cpp RookMoves.cpp BishopMoves.cpp KnightMoves.cpp QueenMoves.cpp KingMoves.cpp PawnMoves.cpp Move.cpp Board.cpp Gameplay.cpp -o chess-game.exe
+
